bd6ff484833c791808289253312c429f3a187a41
highcat <a.sychkov@ad2go.ru>
2010-11-22 20:20:17 +0300

    setup, rename package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='locked_pidfile',
+    version='0.2',
+    description='locked pid-file, fully synchronized',
+    author='Alexander Sychkov',
+    author_email='highcatland@gmail.com',
+    url='http://github.com/highcat/web_tasks',
+    packages=['locked_pidfile']
+    )
