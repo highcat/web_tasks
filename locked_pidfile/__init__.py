e9074791f634a8253a436e6a746e9047a1ef8229
HighCat <a.sychkov@ad2go.ru>
2011-02-02 16:23:06 +0300

    added "verbose" argument. Fixed PidLink output when lock is not acquired

--- locked_pidfile/__init__.py
+++ locked_pidfile/__init__.py
@@ -19,7 +19,7 @@ import time
 # 
 
 
-def __lock_pidfile(filename):
+def __lock_pidfile(filename, verbose=True):
     fd=os.open(filename, os.O_CREAT|os.O_RDWR)
 
     try:
@@ -34,7 +34,7 @@ def __lock_pidfile(filename):
         except ValueError:
             # XXX bad pidfile - clean trash
             os.ftruncate(fd, 0)
-            sys.stderr.write("Bad Pidfile!\nPidfile contains trash: \"%s\"\nPidfile nas been cleaned!\n" % str_pid)
+            if verbose: sys.stderr.write("Bad Pidfile!\nPidfile contains trash: \"%s\"\nPidfile nas been cleaned!\n" % str_pid)
         
         # reLock for write or exit
         fcntl.flock(fd, fcntl.LOCK_EX|fcntl.LOCK_NB)
@@ -57,7 +57,7 @@ class PidLink(object):
     def __init__(self):
         self.pid = None
 
-def lock_pidfile(filename, time_limit=None, pid_output=PidLink()):
+def lock_pidfile(filename, time_limit=None, pid_output=PidLink(), verbose=True):
     """
     Lock pidfile for write, if able.
 
@@ -65,40 +65,40 @@ def lock_pidfile(filename, time_limit=None, pid_output=PidLink()):
         True on success,
         and setspid into PidLink object, if specified
     """
-    ok, pid = __lock_pidfile(filename)
+    ok, pid = __lock_pidfile(filename, verbose=verbose)
+    pid_output.pid = pid    
 
     if ok:
-        pid_output.pid = pid
         return True
     if not ok:
         if not time_limit:
-            sys.stderr.write("can't acquire lock - task is already running, or no access to pidfile (pid='%d')\n" % pid)
+            if verbose: sys.stderr.write("can't acquire lock - task is already running, or no access to pidfile (pid='%d')\n" % pid)
             return False
         else:
-            sys.stderr.write("can't acquire lock - task is running on pid (pid='%d'), checking time limit..\n" % pid)
+            if verbose: sys.stderr.write("can't acquire lock - task is running on pid (pid='%d'), checking time limit..\n" % pid)
             time_file=os.path.getmtime(filename)
             # kill process if lived too long
             try:
                 if pid==0:
-                    sys.stderr.write("read lock set on  \n" % pid)
+                    if verbose: sys.stderr.write("read lock set on  \n" % pid)
                 os.kill(pid, 0)
             except OSError: # can't find PID - so it does not exist
-                ok, pid_2nd = __lock_pidfile(filename)
+                ok, pid_2nd = __lock_pidfile(filename, verbose=verbose)
+                pid_output.pid = pid_2nd                
                 if ok:
-                    pid_output.pid = pid_2nd
                     return True
             else:
-                sys.stderr.write("has been running %f seconds\n" % (time.time()-time_file))
+                if verbose: sys.stderr.write("has been running %f seconds\n" % (time.time()-time_file))
                 if not (time.time()-time_file) <= time_limit * 60:
-                    sys.stderr.write("time limit exceeded!\n")
+                    if verbose: sys.stderr.write("time limit exceeded!\n")
                     try:
-                        sys.stderr.write("killing with SIGTERM...\n")
+                        if verbose: sys.stderr.write("killing with SIGTERM...\n")
                         os.kill(pid, 15)
-                        sys.stderr.write("waiting for process to handle SIGTERM\n")
+                        if verbose: sys.stderr.write("waiting for process to handle SIGTERM\n")
                         for i in range(10):
                             os.kill(pid, 0)                            
                             time.sleep(1)
-                        sys.stderr.write("killing with SIGKILL...\n")
+                        if verbose: sys.stderr.write("killing with SIGKILL...\n")
                         os.kill(pid, 9)
                         for i in range(10):
                             os.kill(pid, 0)
@@ -106,13 +106,13 @@ def lock_pidfile(filename, time_limit=None, pid_output=PidLink()):
                         # NOTE - не 100% гарантия, что запустится после kill (может быть зомби?)
                     except OSError:
                         pass # can't find PID - so it's killed
-                    sys.stderr.write("process has been killed, starting new process\n")
-                    ok, pid_2nd = __lock_pidfile(filename)
+                    if verbose: sys.stderr.write("process has been killed, starting new process\n")
+                    ok, pid_2nd = __lock_pidfile(filename, verbose=verbose)
+                    pid_output.pid = pid_2nd                    
                     if ok:
-                        pid_output.pid = pid_2nd
                         return True
                 else:
-                    sys.stderr.write("time limit is not exceeded pid=%d, stop\n" % pid)
+                    if verbose: sys.stderr.write("time limit is not exceeded pid=%d, stop\n" % pid)
     return False
 
 
@@ -122,7 +122,7 @@ if __name__=='__main__':
     filename = 'myfile.pid'
 
     pl = PidLink()
-    if lock_pidfile(filename, time_limit=10, pid_output=pl):
+    if lock_pidfile(filename, time_limit=10, pid_output=pl, verbose=False):
         print "acquired", pl.pid
         time.sleep(2000)
         print "done"
